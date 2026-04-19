406d28fe19fc01fcf41b72a3a86eed6a72451b13
Harshal Chaudhari <harshalschaudhari@gmail.com>
2026-04-19 12:45:54 +0530

    Change build commands to echo instead of execute

--- jenkins-shared-lib/vars/buildApp.groovy
+++ jenkins-shared-lib/vars/buildApp.groovy
@@ -2,8 +2,8 @@ def call(config) {
     echo "Building ${config.appName} using ${config.buildTool}"
     
     if (config.buildTool == "maven") {
-        sh "mvn clean package"
+         echo  "mvn clean package"
     } else if (config.buildTool == "npm") {
-        sh "npm install && npm run build"
+         echo  "npm install && npm run build"
     }
 }
